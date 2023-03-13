db3fc2b0caeeedce03b6493e02cfe174dc39e183
BrendonW250 <brendonwright16@gmail.com>
2023-03-13 01:30:38 -0400

    header for meme generator app

---
+++ Scrimba-Course-Work/First-project/Meme-Generator/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+/**
+ * Challenge: Build the Header component
+ */
+export default function App() {
+    return (
+        <div>
+            <header className="header">
+                <img src="./images/troll-face.png" alt="troll-face"></img>
+                
+                <h3 className="meme-heading">Meme Generator</h3>
+                
+                <h3 className="react-heading">React Course - Project 3</h3>
+            </header>
+        </div>
+    )
+    
+}
